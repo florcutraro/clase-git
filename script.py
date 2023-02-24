9277aa7078e52b0a546a0032ec964caa9277ced5
Flor Cutraro <florenciacutraro@gmail.com>
2023-02-23 21:45:28 -0300

    Hola!

---
+++ script.py
@@ -0,0 +1,10 @@
+from modulo import suma, resta
+from modulo import Persona
+
+print("Hola mundo!")
+
+print(f'La suma de 1+2 es = {suma(1,2)}')
+
+print(f'La resta de 1-2 es = {resta(1,2)}')
+
+persona_1 = Persona()
